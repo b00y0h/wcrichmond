7cc909d29c6fc6f9d3aa119d16bc762d7cd09266
Bobby Smith <reversetype@gmail.com>
2012-10-24 18:52:19 -0400

    final test?

--- header.php
+++ header.php
@@ -26,7 +26,6 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class($class); ?>>
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   <div id="fb-root"></div>
   <script>(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
