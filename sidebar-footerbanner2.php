a8a378b41c8046076bac1d3250af64d8e12778dd
Bobby Smith <reversetype@gmail.com>
2012-11-02 16:52:20 -0400

    same as last commit. doh

--- sidebar-footerbanner2.php
+++ sidebar-footerbanner2.php
@@ -7,14 +7,14 @@
           margin:0;
         }
       </style>
-      <?php $latest_news = get_posts('numberposts=1'); 
+      <?php $latest_news = get_posts('numberposts=1');
          foreach($latest_news as $post) :
             setup_postdata($post);
        ?>
        <div class="footerNewsBlock">
          <div style="padding:5px 0px;"><a class="blogSnippetLink" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-         <div style="font-size:12px;padding-bottom:5px;font-family: 'Century Gothic', Trebuchet, Helvetica, Arial, sans-serif;"><?php the_date();?></div> 
-         <p style="font-size:13px;">        
+         <div style="font-size:12px;padding-bottom:5px;font-family: 'Century Gothic', Trebuchet, Helvetica, Arial, sans-serif;"><?php the_date();?></div>
+         <p style="font-size:13px;">
          <?php
           $content= get_the_content();
           echo the_excerpt();
@@ -22,19 +22,19 @@
          </p>
        </div>
        <?php endforeach; ?>
-     
+
     </div>
     <div class="newsReadMore" style="font-family: 'Century Gothic', Trebuchet, Helvetica, Arial, sans-serif;font-size:12px">
-      <a href="<? bloginfo('home');?>/news/">READ MORE ></a>
+      <a href="<?php bloginfo('home');?>/news/">READ MORE ></a>
     </div>
   </div>
-  
-  
+
+
   <div id="lifeCareBlock">
-    <a href="<? bloginfo('home');?>/lifestyle/">
+    <a href="<?php bloginfo('home');?>/lifestyle/">
       <img src="<?php bloginfo('template_directory');?>/images/lifestyle-block.png">
     </a>
   </div>
-  
+
   <div style="clear:both;"></div>
 </div>
