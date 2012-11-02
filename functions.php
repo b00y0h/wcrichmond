5924bde18ef6a47245910907b673e3aa00e77395
Bobby Smith <reversetype@gmail.com>
2012-11-02 16:46:19 -0400

    adding stylesheets and js with wp_enqueue_scripts

--- functions.php
+++ functions.php
@@ -13,16 +13,21 @@ function _init_method() {
         wp_enqueue_script( 'jquery' );
         wp_enqueue_script('modernizr', get_bloginfo('template_url') . '/library/js/modernizr.js', null, '1.6', false);
     }
-}    
-
-add_action('wp_print_styles', '_add_stylesheet');
-function _add_stylesheet() {
-	if (!is_admin()) {
-    $styleUrl = get_bloginfo('template_directory') . '/style.css';
-		wp_register_style('StyleSheet', $styleUrl,false,1.1,'all');
-		wp_enqueue_style( 'StyleSheet');
-	}
 }
+
+/**
+ * Enqueue scripts and styles
+ */
+function _add_stylesheet() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    wp_enqueue_script( 'application', get_template_directory_uri() . '/js/application.js', array( 'jquery' ), '20120928', true );
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+}
+add_action( 'wp_enqueue_scripts', '_add_stylesheet' );
 
 
 
@@ -30,7 +35,7 @@ function _add_stylesheet() {
 
 if ( function_exists('register_sidebar') )
   register_sidebar(array(
-    'name' => 'Blog Sidebar',    
+    'name' => 'Blog Sidebar',
     'before_widget' => '<div id="%1$s" class="blogWidget %2$s">',
     'after_widget' => '</div>',
     'before_title' => '<h2>',
@@ -65,7 +70,7 @@ function improved_trim_excerpt($text) {
     $text = strip_tags($text, '<p><img><a><b><strong><i><span>');
     $excerpt_length = 21;
     $words = explode(' ', $text, $excerpt_length + 1);
-      
+
       if (count($words)> $excerpt_length) {
         array_pop($words);
         array_push($words, '...');
@@ -85,47 +90,47 @@ return $initArray;
 add_filter('tiny_mce_before_init', 'add_iframe');
 
 function is_child_of( $page_id, $potential_child_id = '' ) {
- 
+
 	$is_child = false;
- 
+
 	if ( ! is_int( $page_id ) ) {
 		$page = get_page_by_path( $page_id );
 		$page_id = empty( $page ) ? 0 : $page->ID;
 	}
- 
-	if ( empty( $potential_child_id ) ) { 
+
+	if ( empty( $potential_child_id ) ) {
 		$potential_child_id = get_the_ID();
 	}
- 
+
 	$potential_child = get_page( $potential_child_id );
- 
+
 	if ( ! empty( $potential_child ) && is_array( $potential_child->ancestors ) ) {
 		$is_child = in_array( $page_id, $potential_child->ancestors );
 	}
- 
+
 	return $is_child;
 }
 
 function trimstring($string,$length){
-  // Change to the number of characters you want to display 
-  $chars = $length; 
+  // Change to the number of characters you want to display
+  $chars = $length;
 
-  $text = $string." "; 
-  $text = substr($text,0,$chars); 
-  $text = substr($text,0,strrpos($text,' ')); 
-  $text = $text."..."; 
+  $text = $string." ";
+  $text = substr($text,0,$chars);
+  $text = substr($text,0,strrpos($text,' '));
+  $text = $text."...";
 
-  return $text; 
+  return $text;
 
 }
 
 class LiveLifeWellWidget extends WP_Widget {
-  
+
   public function LiveLifeWellWidget() {
     $widget_ops = array('classname' => 'LiveLifeWellWidget', 'description' => 'Live life well tips' );
     $this->WP_Widget('LiveLifeWellWidget', 'Live Life Well Tips Widget', $widget_ops);
   }
-  
+
   public function widget($args, $instance) {
     extract($args);
     /* Our variables from the widget settings. */
@@ -139,12 +144,12 @@ class LiveLifeWellWidget extends WP_Widget {
 		$message6 = $instance['message8'];
       	/* Before widget (defined by themes). */
     echo $before_widget;
-    		
+
     		/* Display the widget title if one was input (before and after defined by themes). */
     if(!empty($title))
     	//echo "<div class='tipsTitle'>". $title . "</div>";
- 
-    
+
+
     ?>
     <script type="text/javascript" src="http://cloud.github.com/downloads/malsup/cycle/jquery.cycle.lite.1.0.min.js"></script>
     <script type="text/javascript" charset="utf-8">
@@ -170,12 +175,12 @@ class LiveLifeWellWidget extends WP_Widget {
     </div>
     <div style="clear:both;"></div>
     <?php
-    
+
       	/* After widget (defined by themes). */
     		echo $after_widget;
-  
+
   }
-  
+
   public function update($newInstance, $oldInstance) {
     $instance = $oldInstance;
     $instance['message1'] = strip_tags( $newInstance['message1'] );
@@ -188,54 +193,54 @@ class LiveLifeWellWidget extends WP_Widget {
     $instance['message8'] = strip_tags( $newInstance['message8'] );
     return $instance;
   }
-  
+
   public function form($instance) {
     $defaults = array( 'message1' => __('', 'livelife-tips'),'message2' => __('', 'livelife-tips'),'message3' => __('', 'livelife-tips'),'message4' => __('', 'livelife-tips'),'message5' => __('', 'livelife-tips'),'message6' => __('', 'livelife-tips') );
-    $instance = wp_parse_args( (array) $instance, $defaults ); 
+    $instance = wp_parse_args( (array) $instance, $defaults );
     ?>
-    
+
     <!-- Message1: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message1' ); ?>"><?php _e('Message 1:', 'livelife-tips'); ?></label>
 			<textarea id="<?php echo $this->get_field_id( 'message1' ); ?>" name="<?php echo $this->get_field_name( 'message1' ); ?>" cols="25" rows="4"><?php echo $instance['message1']; ?></textarea>
 		</p>
-		
+
 		<!-- Message2: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message2' ); ?>"><?php _e('Message 2:', 'livelife-tips'); ?></label>
 			<textarea id="<?php echo $this->get_field_id( 'message2' ); ?>" name="<?php echo $this->get_field_name( 'message2' ); ?>" cols="25" rows="4"><?php echo $instance['message2']; ?></textarea>
 		</p>
-		
+
 		<!-- Message3: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message3' ); ?>"><?php _e('Message 3:', 'livelife-tips'); ?></label>
 			<textarea id="<?php echo $this->get_field_id( 'message3' ); ?>" name="<?php echo $this->get_field_name( 'message3' ); ?>" cols="25" rows="4"><?php echo $instance['message3']; ?></textarea>
 		</p>
-		
+
 		<!-- Message4: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message4' ); ?>"><?php _e('Message 4:', 'livelife-tips'); ?></label>
 			<textarea id="<?php echo $this->get_field_id( 'message4' ); ?>" name="<?php echo $this->get_field_name( 'message4' ); ?>" cols="25" rows="4"><?php echo $instance['message4']; ?></textarea>
 		</p>
-		
+
 		<!-- Message5: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message5' ); ?>"><?php _e('Message 5:', 'livelife-tips'); ?></label>
 			<textarea id="<?php echo $this->get_field_id( 'message5' ); ?>" name="<?php echo $this->get_field_name( 'message5' ); ?>" cols="25" rows="4"><?php echo $instance['message5']; ?></textarea>
 		</p>
-		
+
 		<!-- Message6: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message6' ); ?>"><?php _e('Message 6:', 'livelife-tips'); ?></label>
 			<textarea id="<?php echo $this->get_field_id( 'message6' ); ?>" name="<?php echo $this->get_field_name( 'message6' ); ?>" cols="25" rows="4"><?php echo $instance['message6']; ?></textarea>
 		</p>
-		
+
 		<!-- Message7: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message7' ); ?>"><?php _e('Message 7:', 'livelife-tips'); ?></label>
 			<textarea id="<?php echo $this->get_field_id( 'message7' ); ?>" name="<?php echo $this->get_field_name( 'message7' ); ?>" cols="25" rows="4"><?php echo $instance['message7']; ?></textarea>
 		</p>
-		
+
 		<!-- Message8: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'message8' ); ?>"><?php _e('Message 8:', 'livelife-tips'); ?></label>
@@ -243,7 +248,7 @@ class LiveLifeWellWidget extends WP_Widget {
 		</p>
     <?php
   }
-  
+
 }
 
 
@@ -287,7 +292,7 @@ function posts_for_hr($query) {
 		if(in_array($user_ID,array(4,5,6)) ){
 		  $query->set('author',  4);
 		}
-		
+
 	}
 	return $query;
 }
