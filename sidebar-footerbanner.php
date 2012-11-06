963f838861e942888d0ba688f98376513ae32dec
Bobby Smith <reversetype@gmail.com>
2012-11-06 16:27:14 -0500

    img instead of css bg image to control scalable width

--- sidebar-footerbanner.php
+++ sidebar-footerbanner.php
@@ -8,6 +8,7 @@
         <a href="/contact-us/" id="contact-us" class="module grey-module">
                 <h2 class="header">Contact Us</h2>
                 <p>Click here to request information or schedule a tour.</p>
+                <img src="<?php echo get_bloginfo('template_url'); ?>/images/live-life-well.png" alt="Live Life Well" class="source-image">
         </a>
 
         <a href="/lifecare/" id="life-care" class="module">
