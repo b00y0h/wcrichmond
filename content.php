cd2bfe99588e473e4f48bd7783ee630f159907ea
Bobby Smith <reversetype@gmail.com>
2012-11-09 16:47:58 -0500

    rearrance elements on page

--- content.php
+++ content.php
@@ -1,24 +1,32 @@
-<?php /* if the post has a WP 2.9+ Thumbnail */
-if ( (function_exists('has_post_thumbnail')) && (has_post_thumbnail()) ) { ?>
-<div class="post-thumb">
-
-    <?php if( !is_single() ) { ?><a title="<?php printf(__('Permanent Link to %s', 'wcrichmond'), get_the_title()); ?>" href="<?php the_permalink(); ?>"><?php } ?>
-        <?php the_post_thumbnail('blog-index'); /* post thumbnail settings configured in functions.php */ ?>
-    <?php if( !is_single() ) { ?></a><?php } ?>
-
-</div>
-<?php } ?>
 
 <?php if( is_single() ) { ?>
 
     <h1 class="entry-title"><?php the_title(); ?></h1>
+    <div class="datestamp">
+        <?php the_date(); ?>
+    </div>
+
 
 <?php } else { ?>
 
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'wcrichmond'), get_the_title()); ?>"> <?php the_title(); ?></a></h2>
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'wcrichmond'), get_the_title()); ?>"> <?php the_title(); ?>
+        <?php get_template_part( 'content' , 'meta-header' ); ?>
+    </a>
+    </h2>
+
+
+        <?php /* if the post has a WP 2.9+ Thumbnail */
+        if ( (function_exists('has_post_thumbnail')) && (has_post_thumbnail()) ) { ?>
+        <div class="post-thumb">
+
+            <?php if( !is_single() ) { ?><a title="<?php printf(__('Permanent Link to %s', 'wcrichmond'), get_the_title()); ?>" href="<?php the_permalink(); ?>"><?php } ?>
+                <?php the_post_thumbnail('blog-index'); /* post thumbnail settings configured in functions.php */ ?>
+            <?php if( !is_single() ) { ?></a><?php } ?>
+
+        </div>
+        <?php } ?>
 <?php } ?>
 
-<?php get_template_part( 'content' , 'meta-header' ); ?>
 
 <!--BEGIN .entry-content -->
 <div class="entry-content">
