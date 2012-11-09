f03b197adfd842df92fbf588ce198a3be7036629
Bobby Smith <reversetype@gmail.com>
2012-11-09 02:04:20 -0500

    cleanup single and index

--- index.php
+++ index.php
@@ -1,10 +1,65 @@
 <?php get_header(); ?>
 
-  <div class="blogHeading">
-      <a href="<?php bloginfo('home');?>/news/">
-        <img src="<?php bloginfo('template_directory');?>/images/news_events.png">
-      </a>
-  </div>
+      <h1 class="blogHeading"><a href="<?php bloginfo('home');?>/news/">
+        Around the Community
+      </a></h1>
+
+
+            <!--BEGIN #primary .hfeed-->
+            <div id="primary" class="hfeed">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <!--BEGIN .hentry -->
+                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+                    <?php
+                    $format = get_post_format();
+                    get_template_part( 'content', $format );
+                    ?>
+
+                <!--END .hentry-->
+                </div>
+
+                <?php endwhile; ?>
+
+                <!--BEGIN .navigation .page-navigation -->
+                <div class="navigation page-navigation">
+                    <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
+                    <div class="nav-next"><?php next_posts_link(__('&larr; Older Entries', 'wcrichmond')) ?></div>
+                    <div class="nav-previous"><?php previous_posts_link(__('Newer Entries &rarr;', 'wcrichmond')) ?></div>
+                    <?php } ?>
+                <!--END .navigation .page-navigation -->
+                </div>
+
+            <?php else : ?>
+
+                <!--BEGIN #post-0-->
+                <div id="post-0" <?php post_class(); ?>>
+
+                    <h2 class="entry-title"><?php _e('Error 404 - Not Found', 'wcrichmond') ?></h2>
+
+                    <!--BEGIN .entry-content-->
+                    <div class="entry-content">
+                        <p><?php _e("Sorry, but you are looking for something that isn't here.", "wcrichmond") ?></p>
+                    <!--END .entry-content-->
+                    </div>
+
+                <!--END #post-0-->
+                </div>
+
+            <?php endif; ?>
+            <!--END #primary .hfeed-->
+            </div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
+
+
+
+
+
 
   <div class="blogContentWrap">
 
@@ -71,5 +126,3 @@
     <?php get_sidebar('blog'); ?>
   </div>
 </div>
-
-<?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,16 @@
-<?php 	/* Widgetized sidebar, if you have the plugin installed. */
-		//if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>  
-<?php //endif; ?>
+        <!--BEGIN #sidebar .aside-->
+        <div id="sidebar" class="aside">
+
+        <?php
+
+            /* Widgetised Area */
+            if( is_page() ) {
+                dynamic_sidebar( 'sidebar-page' );
+            } else {
+                dynamic_sidebar( 'sidebar-blog' );
+            }
+
+        ?>
+
+        <!--END #sidebar .aside-->
+        </div>
