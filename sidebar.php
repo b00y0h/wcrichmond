5a4925f5c44392f9948896db79dee39739fe8741
Bobby Smith <reversetype@gmail.com>
2012-11-12 15:38:37 -0500

    FIX: syntax error.
    wasn't using <?php <==== nooob

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,4 @@
+
 <!--BEGIN #sidebar .aside-->
 <div id="sidebar" class="aside">
 
@@ -13,31 +14,20 @@
       <ul>
           <?php
           /**/
+
           $years = $wpdb->get_col("SELECT DISTINCT YEAR(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY post_date DESC");
+
           if( is_array($years)){
             foreach($years as $year) {
             ?>
                 <li><a href="<?php echo get_year_link($year); ?> "><?php echo $year; ?></a>
 
                     <ul>
-                    <?  $months = $wpdb->get_col("SELECT DISTINCT MONTH(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' AND YEAR(post_date) = '".$year."' ORDER BY post_date DESC");
+                    <?php  $months = $wpdb->get_col("SELECT DISTINCT MONTH(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' AND YEAR(post_date) = '".$year."' ORDER BY post_date DESC");
                         if( is_array($months) ){
                           foreach($months as $month) {
                         ?>
-                        <li><a href="<?php echo get_month_link($year, $month); ?>"><?php echo date( 'F', mktime(0, 0, 0, $month) );?></a>
-                            <ul>
-                            <?  $days = $wpdb->get_col("SELECT DISTINCT DAY(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' AND MONTH(post_date) = '".$month."' AND YEAR(post_date) = '".$year."' ORDER BY post_date DESC");
-                                if(is_array($days)){
-                                  foreach($days as $day) {
-                                ?>
-                                <li><a href="<?php echo get_day_link($year, $month, $day); ?>"><?php echo $day;?></a></li>
-                                <?php
-                                  }
-                                }
-                                ?>
-                            </ul>
-
-                        </li>
+                        <li><a href="<?php echo get_month_link($year, $month); ?>"><?php echo date( 'F', mktime(0, 0, 0, $month) );?></a></li>
                         <?php
                           }
                         }?>
