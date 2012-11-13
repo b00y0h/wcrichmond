12e390e06761ca30d60a7779d70ed462da383e79
Bobby Smith <reversetype@gmail.com>
2012-11-13 14:47:38 -0500

    illegal html. can't have div inside of h2

--- content-meta-header.php
+++ content-meta-header.php
@@ -1,5 +1,1 @@
-<!--BEGIN .entry-meta .entry-header-->
-<div class="entry-meta entry-header">
-    <span class="published"><?php the_time( get_option('date_format') ); ?></span>
-<!--END .entry-meta entry-header -->
-</div>
+    <span class="entry-meta entry-header published"><?php the_time( get_option('date_format') ); ?></span>
