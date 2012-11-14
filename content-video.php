3efdc0086267237a55165daedaa58566e5ab52d6
Bobby Smith <reversetype@gmail.com>
2012-11-14 01:31:49 -0500

    add href to image

--- content-video.php
+++ content-video.php
@@ -6,9 +6,10 @@
 <div class="entry-content">
 
     <?php
-    if ( has_post_thumbnail() ) {
-    the_post_thumbnail();
-} ?>
+    if ( has_post_thumbnail() ) { ?>
+    <a href="<?php echo get_post_meta($post->ID, '_video_link', true); ?>" class="thumbnail">
+    <?php the_post_thumbnail();} ?>
+    </a>
     <h2 class="entry-title"><?php the_title(); ?></h2>
 
     <?php the_content(); ?>
