c46a143197eff2548ecbdf63c4fb6a4f4c08231b
Bobby Smith <reversetype@gmail.com>
2012-11-28 15:20:08 -0500

    remove fb-root from header and let fb put it at the bottom of html

--- header.php
+++ header.php
@@ -34,7 +34,6 @@
 <!--[if lt IE 7]>
 <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
 <![endif]-->
-<div id="fb-root"></div>
 <script>(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
