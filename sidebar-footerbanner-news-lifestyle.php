41f86f313ef98b273c4ee35f81fcc41fd470c191
Bobby Smith <reversetype@gmail.com>
2013-01-04 16:29:24 -0500

    prepping for addition of new footer banner

---
+++ sidebar-footerbanner-news-lifestyle.php
@@ -0,0 +1,11 @@
+<div class="diptic">
+
+    <a href="/lifestyle/" id="lifestyle" class="module">
+        <h2 class="transparent header">LifeStyle</h2>
+    </a>
+
+    <a href="/lifecare/" id="life-care" class="module">
+        <h2 class="transparent header">Life Care</h2>
+    </a>
+
+</div> <!-- .triptic -->
