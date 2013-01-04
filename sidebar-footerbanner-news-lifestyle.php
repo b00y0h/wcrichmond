ba21a1114d793c706a2b0c739f736b23102c5139
Bobby Smith <reversetype@gmail.com>
2013-01-04 17:06:52 -0500

    lifecare footer template layout

--- sidebar-footerbanner-news-lifestyle.php
+++ sidebar-footerbanner-news-lifestyle.php
@@ -1,11 +1,33 @@
-<div class="diptic">
+<div class="diptic last-child-large">
 
-    <a href="/lifestyle/" id="lifestyle" class="module">
+  <div id="newsEventsBlock" class="module">
+    <h3 class="blockTitle"><a href="<?php bloginfo('home');?>/calendar">News &amp; Calendar</a></h3>
+    <div class="newsFeed">
+      <?php $latest_news = get_posts('numberposts=1');
+         foreach($latest_news as $post) :
+            setup_postdata($post);
+       ?>
+       <div class="footerNewsBlock">
+         <div><a class="blogSnippetLink" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+         <div><?php the_date();?></div>
+         <p>
+         <?php
+          $content= get_the_content();
+          echo the_excerpt();
+         ?>
+         </p>
+       </div>
+       <?php endforeach; ?>
+
+    </div>
+    <div class="newsReadMore">
+      <a href="<?php bloginfo('home');?>/news/">READ MORE ></a>
+    </div>
+  </div>
+
+
+    <a href="/lifestyle/" id="lifestyle" class="module desk-image">
         <h2 class="transparent header">LifeStyle</h2>
     </a>
 
-    <a href="/lifecare/" id="life-care" class="module">
-        <h2 class="transparent header">Life Care</h2>
-    </a>
-
-</div> <!-- .triptic -->
+</div> <!-- .diptic -->
