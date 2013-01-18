f4cdb94abdc7b8bc116dd02afaa612f9f772e9c6
Bobby Smith <reversetype@gmail.com>
2013-01-17 21:53:33 -0500

    spacing for modules on home page

--- js/application.js
+++ js/application.js
@@ -7,40 +7,40 @@
     $(document).ready(function(){
         $('.thumbnail-wrap .button').each(function() {
 
-        		var vid_string = $(this).attr('href');
-		        var vid_html = "";
-		        if(vid_string.indexOf('youtube') !== -1) {
+                var vid_string = $(this).attr('href');
+                var vid_html = "";
+                if(vid_string.indexOf('youtube') !== -1) {
 
-		            vid_string = vid_string.replace('www.', '').replace('http://youtube.com/watch?v=', '');
-		            vid_html = '<iframe class="vid-frame" width="294" height="164" src="http://www.youtube.com/embed/' + vid_string + '?rel=0&amp;autoplay=0&amp;wmode=transparent" frameborder="0"></iframe>';
+                    vid_string = vid_string.replace('www.', '').replace('http://youtube.com/watch?v=', '');
+                    vid_html = '<iframe class="vid-frame" width="294" height="164" src="http://www.youtube.com/embed/' + vid_string + '?rel=0&amp;autoplay=0&amp;wmode=transparent" frameborder="0"></iframe>';
 
-		        } else if(vid_string.indexOf('youtu.be') !== -1) {
+                } else if(vid_string.indexOf('youtu.be') !== -1) {
 
-		            vid_string = vid_string.replace('www.', '').replace('http://youtu.be/', '');
-		            vid_html = '<iframe class="vid-frame" width="294" height="164" src="http://www.youtube.com/embed/' + vid_string + '?rel=0&amp;autoplay=0&amp;wmode=transparent" frameborder="0"></iframe>';
+                    vid_string = vid_string.replace('www.', '').replace('http://youtu.be/', '');
+                    vid_html = '<iframe class="vid-frame" width="294" height="164" src="http://www.youtube.com/embed/' + vid_string + '?rel=0&amp;autoplay=0&amp;wmode=transparent" frameborder="0"></iframe>';
 
-		        } else if(vid_string.indexOf('vimeo') !== -1) {
+                } else if(vid_string.indexOf('vimeo') !== -1) {
 
-		            vid_string = vid_string.replace('www.', '').replace('http://vimeo.com/', '').replace('https://vimeo.com/', '');
-		            vid_html = '<iframe class="vid-frame" src="http://player.vimeo.com/video/' + vid_string + '?portrait=0&amp;autoplay=0" width="294" height="164" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
-		        }
+                    vid_string = vid_string.replace('www.', '').replace('http://vimeo.com/', '').replace('https://vimeo.com/', '');
+                    vid_html = '<iframe class="vid-frame" src="http://player.vimeo.com/video/' + vid_string + '?portrait=0&amp;autoplay=0" width="294" height="164" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+                }
 
-		        $(this).fadeOut(300);
-		        //$(this).parent().css('background', '#E6E6E6').find('img').fadeOut(300);
-						
-		        $(this).parent().append(vid_html);
+                $(this).fadeOut(300);
+                //$(this).parent().css('background', '#E6E6E6').find('img').fadeOut(300);
 
-		        //return false;
-		    });
+                $(this).parent().append(vid_html);
+
+                //return false;
+            });
 
 
 
         // redraw certain items on browser resize so that the css font
         // size value of 'vw' work
-        var causeRepaintsOn = $("#act-now h2");
+        // var causeRepaintsOn = $("#act-now h2");
 
         $(window).resize(function() {
-            causeRepaintsOn.css("z-index", 1);
+            // causeRepaintsOn.css("z-index", 1);
         });
 
 
